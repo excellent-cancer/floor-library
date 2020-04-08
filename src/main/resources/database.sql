2fbd081a296b2a0472ddf91a4688af0184a90452
XyParaCrim <xylome@qq.com>
2020-04-09 02:45:54 +0800

    feat: save sql file

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -2,7 +2,21 @@ create database experimental;
 
 drop table if exists experimental.owner;
 
+drop table if exists experimental.owner_link;
 
 create table experimental.owner(
-    id bigint primary key auto_increment
+    id bigint unsigned not null auto_increment,
+    username varchar(32) not null unique,
+    organization varchar(128) not null,
+    primary key (id)
+);
+
+
+
+create table experimental.owner_link(
+    id bigint unsigned not null auto_increment,
+    url varchar(128) not null,
+    name varchar(32) not null,
+    owner_id bigint unsigned not null references experimental.owner(id),
+    primary key (id)
 );
