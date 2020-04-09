90742e9b96651eab3fa47fa92833c4c6eaff3823
XyParaCrim <xylome@qq.com>
2020-04-09 16:42:50 +0800

    feat: add logback.xml log setting file
    The console output has no color, when the unit test is caused by unknown reasons.

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -11,8 +11,6 @@ create table experimental.owner(
     primary key (id)
 );
 
-
-
 create table experimental.owner_link(
     id bigint unsigned not null auto_increment,
     url varchar(128) not null,
